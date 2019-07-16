36faa8e7d6ca5ad45278f98821cad0f6a1cd6f18
zengning <0380008937@hxct.com>
2019-07-16 15:21:39 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ function resolve (dir) {
 
 module.exports = {
   publicPath: baseUrl,
-  outputDir: 'doc',
+  outputDir: 'docs',
   lintOnSave: undefined,
   runtimeCompiler: true, // 运行时版本是否需要编译
   transpileDependencies: [], // 默认babel-loader忽略mode_modules，这里可增加例外的依赖包名
